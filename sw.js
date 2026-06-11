c20dd033e64e4cbc079d8212160283940c8272fb
Prashast Vaidya <prashast.vaidya@gmail.com>
2026-06-11 16:28:43 -0700

    Update service worker cache version to invalidate old cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aurahabit-cache-v1';
+const CACHE_NAME = 'aurahabit-cache-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -8,11 +8,26 @@ const ASSETS = [
   './icon.png'
 ];
 
-// Cache all core application assets upon installation
+// Cache all core assets upon installation
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate listener to delete old caches automatically
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
